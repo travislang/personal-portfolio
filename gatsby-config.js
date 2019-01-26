dd4e4db3b4c236ec497ebd3ab482c579ac0a24bd
Travis Lang <tlang50@icloud.com>
2019-01-26 01:27:38 -0600

    fix package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,8 +26,5 @@ module.exports = {
         display: `minimal-ui`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ],
 }
