c0a077998fd01444357be2f9d6969c6e1e41faba
Travis Lang <tlang50@icloud.com>
2019-01-25 19:32:56 -0600

    add logoes to skills page with tooltips

--- src/components/projects/projectCard.js
+++ src/components/projects/projectCard.js
@@ -8,6 +8,7 @@ import ReactTooltip from 'react-tooltip'
 //icons
 import { FaGithub } from 'react-icons/fa';
 import { MdExitToApp } from 'react-icons/md';
+import { FaExternalLinkAlt } from 'react-icons/fa';
 import { FaInfoCircle } from 'react-icons/fa';
 
 import { FaNodeJs } from 'react-icons/fa';
@@ -51,13 +52,18 @@ class ProjectCard extends Component {
     }
 
     render() {
+        const {project} = this.props;
         let backgroundColor;
         this.state.moreInfo ? backgroundColor = '#424242' : backgroundColor = '#e0e0e0';
         return (
             <div className={style.root} style={{backgroundColor: backgroundColor}}>
                 <div className={style.actions}>
-                    <MdExitToApp data-tip="View App" className={style.actionIcon} />
-                    <FaGithub data-tip="View Source on Github" className={style.actionIcon} />
+                    <a href={project.demo} target='_blank'>
+                        <FaExternalLinkAlt data-tip="View App" className={style.actionIcon} />
+                    </a>
+                    <a href={project.github} target='_blank'>
+                        <FaGithub data-tip="View Source on Github" className={style.actionIcon} />
+                    </a>
                     <FaInfoCircle data-tip="More Info" onClick={this.handleClick} className={style.actionIcon} />
                 </div>
                 {!this.state.moreInfo ?
@@ -66,8 +72,8 @@ class ProjectCard extends Component {
                             <Image />
                         </div>
                         <div className={style.innerText}>
-                            <h2 className={style.cardTitle}>CoinDock</h2>
-                            <p className={style.cardDescription}>A cross browser cryptocurrency tracking application with real time prices</p>
+                            <h2 className={style.cardTitle}>{project.title}</h2>
+                            <p className={style.cardDescription}>{project.description}</p>
                             <hr />
                             <h3 className={style.cardTitleSecondary}>Built Using</h3>
                             <div>
@@ -80,13 +86,13 @@ class ProjectCard extends Component {
                     :
                     <div className={style.innerCard}>
                         <p style={{color: '#eeeeee'}}>
-                            This application uses webSockets to stream real time cryptocurrency prices to the user with the ability to create/delete portfolios, add/remove cryptocurrencies, drag and drop to organize cryptocurrencies within portfolios, and set price alerts on specific cryptocurrencies.
+                            {project.appInfo1}
                         </p>
                         <p style={{ color: '#eeeeee' }}>
-                            Alerts are constantly monitored on the server and when required an alert will be sent using Push Notifications. This is accomplished utalizing the Push API and service workers.
+                            {project.appInfo2}
                         </p>
                         <p style={{ color: '#eeeeee' }}>
-                            Checkout the readme on Github to see a full list of all technologies used.
+                            {project.appInfo3}
                         </p>
                     </div>
                 }
--- src/components/projects/projectsPage.js
+++ src/components/projects/projectsPage.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import projects from './projectsInfo';
 
 import style from './projectsPage.module.css';
 import ProjectCard from './projectCard';
 
 const ProjectsPage = () => (
-    <div className={style.root}
-    >
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+    <div className={style.root}>
+        {projects.map(project => {
+            return <ProjectCard project={project} />
+        })}
     </div>
 )
 
