6363d36debf2c98b8f8a68eb7ea89b00e60d0d77
Travis Lang <tlang50@icloud.com>
2019-02-20 15:24:04 -0600

    add new project

--- src/components/projects/projectsInfo.js
+++ src/components/projects/projectsInfo.js
@@ -10,6 +10,15 @@ const projects = [
         appInfo3: 'Checkout the readme on Github to see a full list of all technologies used.'
     },
     {
+        title: 'ResponseGen',
+        demo: 'https://github.com/travislang/patent-app',
+        github: 'https://github.com/travislang/patent-app',
+        description: 'A CMS and template generation application for patent attorneys',
+        appInfo1: 'This is a full stack application built for a patent attorney that manages patent applications and generates response documents.',
+        appInfo2: 'The user can create, edit, delete applications, select applicable templates, and in line edit those templates to suit the needs of the specific use case.',
+        appInfo3: 'Once a full document has been generated based on editable templates the user can export it into a complete docx format.'
+    },
+    {
         title: 'Weather App',
         demo: 'https://github.com/travislang/react-native-weather-app',
         github: 'https://github.com/travislang/react-native-weather-app',
@@ -27,15 +36,6 @@ const projects = [
         appInfo2: 'The application allows a user to add new tasks, complete tasks, and delete tasks.  Tasks are automatically reordered to place completed tasks at the bottom of the list. All of the tasks and whether or not they have been completed is stored in a backend database.',
         appInfo3: 'This application used jQuery. Checkout the readme on Github for more detailed information.'
     },
-    {
-        title: 'Feedback App',
-        demo: 'https://tasks-5.herokuapp.com/',
-        github: 'https://github.com/travislang/feedback',
-        description: 'A simple feedback form that has seperate views with state, and on submit the feeback is saved in a database',
-        appInfo1: 'This is a basic react feeback project that directs a user to enter information across multiple pages. The application shows the step the user is on and how many more pages are left before being allowed to submit the feedback.',
-        appInfo2: 'As the user moves through the pages they can see all of the information that has been entered.  Once the user submits the feeback the data is saved in a backend database using postreSQL.',
-        appInfo3: 'Checkout the readme on Github for more detailed information.'
-    }
 ]
 
 export default projects
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -23,21 +23,21 @@ export const query = graphql`
             }
         }
     }
-    image2: file(relativePath: { eq: "weather-app.png" }) {
+    image2: file(relativePath: { eq: "responsegen-mockup.png" }) {
         childImageSharp {
             fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
             }
         }
     }
-    image3: file(relativePath: { eq: "todo-mockup.png" }) {
+    image3: file(relativePath: { eq: "weather-app.png" }) {
         childImageSharp {
             fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
             }
         }
     }
-    image4: file(relativePath: { eq: "feedback-mockup.png" }) {
+    image4: file(relativePath: { eq: "todo-mockup.png" }) {
         childImageSharp {
             fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
